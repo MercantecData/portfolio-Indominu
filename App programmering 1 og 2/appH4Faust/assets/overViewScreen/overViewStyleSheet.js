3e96652709d37e11ec9871ac3f7d9fab7e475023
Mathias H. Faust <math7753@edu.mercantec.dk>
2020-01-20 09:15:21 +0100

    list in list done

---
+++ App programmering 1 og 2/appH4Faust/assets/overViewScreen/overViewStyleSheet.js
@@ -0,0 +1,22 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+
+  background:{
+    backgroundColor:'#f6f6fa',
+    height: '100%',
+    width: '100%'
+  },
+
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#f6f6fa',
+    height: '100%',
+    width: '100%'
+  },
+
+  innerContainer: {
+    alignItems: 'center',
+  }
+});
